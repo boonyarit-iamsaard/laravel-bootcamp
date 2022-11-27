4cbbb625ba3cea8a4e116810568aed9f95209174
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2022-11-27 14:58:32 +0700

    build: implement laravel breeze with react

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    // For running vite with Laravel Sail in WSL2
+    server: {
+        hmr: {
+            host: 'localhost',
+        }
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.jsx',
             refresh: true,
         }),
+        react(),
     ],
 });
